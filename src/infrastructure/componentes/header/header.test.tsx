92eb2d9945ae76130bd55eecd38fbbd7bc4a307a
Liluminalia <peloazul4@gmail.com>
2022-11-20 15:37:17 +0100

    assing header file and test

---
+++ src/infrastructure/componentes/header/header.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { Header } from './header';
+
+describe('Given Header component', () => {
+    describe('When we render the component', () => {
+        test('Then it should display "Home"', () => {
+            render(
+                <>
+                    <Router>
+                        <Header />
+                    </Router>
+                </>
+            );
+            const element = screen.getByText(/Home/i);
+            expect(element).toBeInTheDocument();
+        });
+    });
+});
---
+++ src/infrastructure/componentes/header/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <>
+            <header>
+                <h1 className="header__title">Milk & Cookies</h1>
+            </header>
+        </>
+    );
+}
