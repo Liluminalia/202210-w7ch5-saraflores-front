6900ea44ce72d760852886c6d644402e2031c5af
Liluminalia <peloazul4@gmail.com>
2022-11-20 15:06:48 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './infrastructure/componentes/app/app';
+import { appStore } from './infrastructure/store/store';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { MemoryRouter as Router } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <Router>
+            <Provider store={appStore} children={undefined}>
+                <App />
+            </Provider>
+        </Router>
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
