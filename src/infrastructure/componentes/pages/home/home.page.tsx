3e71978ddb04e2c76b8f5858914fb578d6bf3117
Liluminalia <peloazul4@gmail.com>
2022-11-20 17:41:44 +0100

    fixing tests

--- src/infrastructure/componentes/pages/home/home.page.tsx
+++ src/infrastructure/componentes/pages/home/home.page.tsx
@@ -1,5 +1,15 @@
+import { Provider } from 'react-redux';
+import { RobotsList } from '../../../../features/robots/components/robots.list/robots.list';
+import { useRobots } from '../../../../features/robots/hook/use.robots';
+import { appStore } from '../../../store/store';
+
 function Home() {
-    return <main className="main">Home page</main>;
+    const { robots } = useRobots();
+    return (
+        <Provider store={appStore}>
+            <RobotsList item={robots}></RobotsList>
+        </Provider>
+    );
 }
 
 export default Home;
