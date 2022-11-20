f7e12b911ab77d8b07230fbb0469b25c18238801
Liluminalia <peloazul4@gmail.com>
2022-11-20 16:12:43 +0100

    ✅ fixing detail page test

--- src/infrastructure/componentes/pages/details/details.page.test.tsx
+++ src/infrastructure/componentes/pages/details/details.page.test.tsx
@@ -5,7 +5,7 @@ import DetailsPage from './details.page';
 
 describe('Given Details page', () => {
     describe('When we render the page', () => {
-        test('Then it should display "PVP"', () => {
+        test('Then it should display "Velocidad"', () => {
             render(
                 <>
                     <Router>
@@ -13,7 +13,7 @@ describe('Given Details page', () => {
                     </Router>
                 </>
             );
-            const element = screen.getByText(/PVP/i);
+            const element = screen.getByText(/Velocidad/i);
             expect(element).toBeInTheDocument();
         });
     });
