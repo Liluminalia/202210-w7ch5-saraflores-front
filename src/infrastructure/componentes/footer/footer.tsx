ce16332c40465c320d73b18e00230c86f57ae550
Liluminalia <peloazul4@gmail.com>
2022-11-20 15:33:16 +0100

    updating footer and layout for tests in sonar

--- src/infrastructure/componentes/footer/footer.tsx
+++ src/infrastructure/componentes/footer/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <footer className="footer">
-            <div className="footer__address">Sara Flores, Icommerce from </div>
+            <div className="footer__address">Sara Flores, from </div>
             <div className="footer__coders">CODERS</div>
         </footer>
     );
